d24ccc529e72c5a36c0e0845d1244df8fddbeea5
apotemkin <apotemkin@yandex-team.ru>
2020-07-16 13:47:36 +0300

    Body search capability added.

--- netfox/Core/NFXAssets.swift
+++ netfox/Core/NFXAssets.swift
@@ -12,6 +12,8 @@ enum NFXAssetName {
     case close
     case info
     case statistics
+    case rightArrow
+    case leftArrow
 }
 
 class NFXAssets
@@ -24,6 +26,8 @@ class NFXAssets
             case .close: return getCloseImageBase64()
             case .info: return getInfoImageBase64()
             case .statistics: return getStatisticsImageBase64()
+            case .rightArrow: return getRightArrowImageBase64()
+            case .leftArrow: return getLeftArrowImageBase64()
             }
         }()
 
@@ -48,6 +52,16 @@ class NFXAssets
     class func getStatisticsImageBase64() -> String
     {
         return "iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAAflBMVEUAAADsXijsXijsXijsXijtXSfsXijsXSjsXijsXijsXSbsXijtXSjsXijsXifsXijsXiftXifsXifsXCfsXifsXifsXifsXifsXSftXSfsWyjsXijsXifsXifsXijtXifsXiftXybsXSfsXijtXSftXifsWibsXijuYCfsXijgd1NoAAAAKXRSTlMANNuwVBbuh/CUA9UG+t/QvCz0D+jYy3RKOQrlxqmOgGUgv7VvVwxbHXzMYTYAAAEjSURBVDjL7ZRJcoMwEEVlEgkhMU8GzOgp+fe/YChXxQSqK27vvPBbQfdbSD1IkMSxYONb6/Nta+n4B4XWZFjgCQTCZMciCWfZEUyctzzTDH3GlgfgSMmTCuXWlREQUXILbIcz0UijiZC/AiC4/FXNgOBMXdCckI4jEPvLYLdId1Q1pgOi+dZZhDRZjlASpfO7MsXZv32OCNxbdh9gT9U5BoLqN1orlNI0HnRNNiUHvCUsj8htiJOkO/gdlauMi3sRiXabTSc0ULFnI2kvLzaiLyjn1SeLKsdzb92OoA7DmooLClMURnDxAI/ruphxua4+HDTP9qAyazMFj+VK4zhGMmwXqplXdV7URsF96Mr7tjywe6jr8ndV6P+Ru0KuNrHoVukfuacsKwHjtUEAAAAASUVORK5CYII="
+    }
+    
+    class func getRightArrowImageBase64() -> String
+    {
+        return "iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAACgUlEQVRoQ+2ZzWoTURzFzz+StoJoEbIwM/cSQvAFBHXhwp2pTd241UfwW8SFO1fFbx+h4CMIfisUdeELCHbTSTIICS6sNBND85cBlcnIRHLnfiTQWc+9c37n/A93MiHM+EUzrh+7AK4TVE6gVCrtKxaLXhiGXwAMXYEoAUh56Ahz4RWARYDW5+e3Gxsb3767gFAC8H1vjQjn/wgmwse5uV7dBYQSgJT+KjPfSDruCkIJoFY7uL/f3/scwHHXEEoAsehqtXpgMOg/cw2hDDAOghkfFhZ6SzY6kQtgGiByAyQg4k4cS3bCRhJaAFxCaAP4H0QU9evdbndL92GnFcAFhHYA2xBGABIQLwAcTRdb5zgZA7AFYRRgHASA971efylvsY0DmIawAmASwhpADFGpVBZ3dgbxiT1S7HicouhnvdPp/Jj0nLAKMA6CmVdbrfDm1APEAoXwbgO4Nfpbgp8EQXhu6gGEKF8H6E5K6DZQONlsNj9NNUCWeGZabrVa7yYVH99vrQNCeNcA3E07n0e8NYAM8T1mOq3q/N8vIiqxTbJGCO8qgHupNVrEG08gS/xwiOV2u/12EiOy7jXWASG8KwDup53XKd5YAlniCwU0Njfbb3Q4b6wDNsVrT0BK7zIzHqTHxoTz2hPIEk+ElSBov9Y5NiOvIDo2ltK/xMwPU3tFRGiYFK9lhFyKzw0gpX+RmR+5cD53B3zfv0DEj/8VzytBEMb/3li5lA4yz/MOFwr4nHoZjIjsilceISnLJ5hpPWFxLP5MEIQvrdieeIhSAgD2CFF+CtApImwBfNaFeOUEfhtAvu/Xoij6mvfbTp7UVBPI80yta3cBtNqpsNnMJ/ALjQd9QEF/zVwAAAAASUVORK5CYII="
+    }
+    
+    class func getLeftArrowImageBase64() -> String
+    {
+        return "iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAACdElEQVRoQ+2YzWoTYRSG3xMYJxHrQpKFM5PppoI3IPhDQWttBS9B8BL8B3du3Ijg/yXYrQtt1XQpuFDBG9CSxTfz1UVAVATbBufISKNxyDRk5vtJoFnPfDzP+54zk4Qw4R+acH7sCthu0GoD9Xp9ynXdg1LKTwC4SBjWBMLQOwPQU2ZMAbwaRevnAPwaVcKKQLPpLQD0DEC1B0zEs0Ksvxl7gTR5ZnreD5+OT5LgsJTy41gL5MCDmS7Gcfx4VPj0emMjFIbePDMtZ5IvBW9MQBe8EYE8eCK6JET8qMjY9N+jdYTC0D/NjJXs2KiC19rADvCXhYgflk3+7+NX1UH952zDpwtb+69uIqXwWhrIh8cVIeQD1YEp3YHpaX8uSf7MfCZ5PfBKG8iDB3A1iuR91ckr3QFb8Eoa8H3/VKWCF9mxAXAtiuQ9XckracA2fKkGgiA4ScQvByR/PYrkXd3Jl2qg2WweAZLXtuELNxCG3hIznc+kfCuK5E1TyZdqIAi820R0IwP73nHchXa7/c2kRKEXWaPR2Fet7mkBOGFbopBACp3+o1Ctui0iHLcpUVhgiMQ7x3EXTYxTKYFxkCgtYFtCiUAqMTNzYP/GRu3VgJ3QOk7KBGxJKBUYIvHWcdyzqhdbuUBPYmur1mLGscwjVrmEFgGTEtoEhkm47s/FtbUv38t+7dAqYEJCu4BuCSMCOiWMCfQkNjdrqwCOZp9ORXfCqMBOEkR0R4g4+xtj6I4bF8iTYMaTOJYXhhJnLrAi8E9i7wrAswC+EiXzQnz+MDEC26AVz/MOdbtd2el0fowKn15vrYEisIPu2RVQlWTRcya+gd/tYzVAimIG5gAAAABJRU5ErkJggg=="
     }
     
 }
--- netfox/Core/NFXRawBodyDetailsController.swift
+++ netfox/Core/NFXRawBodyDetailsController.swift
@@ -10,10 +10,31 @@
 import Foundation
 import UIKit
 
-class NFXRawBodyDetailsController: NFXGenericBodyDetailsController
+class NFXRawBodyDetailsController: NFXGenericBodyDetailsController, UISearchBarDelegate
 {
     var bodyView: UITextView = UITextView()
+    private var originalString: String?
     private var copyAlert: UIAlertController?
+    private var timer: Timer?
+    private let nextButton = UIButton(type: .system)
+    private let prevButton = UIButton(type: .system)
+    private var currentResult: Int = 0 {
+        didSet {
+            if searchResults.count == 0 {
+                barItem.title = ""
+            } else {
+                barItem.title = "\(currentResult + 1) из \(searchResults.count)"
+            }
+            
+        }
+    }
+    private let barItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+
+    private var searchResults: [NSTextCheckingResult] = []
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
     
     override func viewDidLoad()
     {
@@ -21,26 +42,109 @@ class NFXRawBodyDetailsController: NFXGenericBodyDetailsController
         
         self.title = "Body details"
         
-        self.bodyView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-        self.bodyView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        let searchBar = UISearchBar()
+        searchBar.backgroundImage = UIImage()
+        searchBar.placeholder = "Поиск"
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.delegate = self
+        searchBar.backgroundColor = .clear
+        self.view.addSubview(searchBar)
+        
+        nextButton.setImage(UIImage(data: NFXAssets.getImage(.rightArrow)) , for: .normal)
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        nextButton.addTarget(self, action: #selector(nextTapped), for: .touchUpInside)
+        nextButton.isEnabled = false
+        nextButton.imageEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        nextButton.imageView?.contentMode = .scaleAspectFit
+        view.addSubview(nextButton)
+        
+        prevButton.setImage(UIImage(data: NFXAssets.getImage(.leftArrow)) , for: .normal)
+        prevButton.translatesAutoresizingMaskIntoConstraints = false
+        prevButton.addTarget(self, action: #selector(prevTapped), for: .touchUpInside)
+        prevButton.isEnabled = false
+        prevButton.imageEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        prevButton.imageView?.contentMode = .scaleAspectFit
+        view.addSubview(prevButton)
+        
+        self.bodyView.translatesAutoresizingMaskIntoConstraints = false
         self.bodyView.backgroundColor = UIColor.clear
         self.bodyView.textColor = UIColor.NFXGray44Color()
 		self.bodyView.textAlignment = .left
         self.bodyView.isEditable = false
         self.bodyView.isSelectable = false
         self.bodyView.font = UIFont.NFXFont(size: 13)
+        self.bodyView.keyboardDismissMode = .onDrag
+        self.view.addSubview(self.bodyView)
+        
+        navigationItem.rightBarButtonItem = barItem
+
+        NSLayoutConstraint.activate([
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.topAnchor.constraint(equalTo: view.topAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: prevButton.leadingAnchor),
+            
+            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: searchBar.centerYAnchor),
+            nextButton.heightAnchor.constraint(equalTo: searchBar.heightAnchor, multiplier: 0.7),
+            nextButton.widthAnchor.constraint(equalTo: nextButton.heightAnchor),
+            
+            prevButton.trailingAnchor.constraint(equalTo: nextButton.leadingAnchor),
+            prevButton.centerYAnchor.constraint(equalTo: searchBar.centerYAnchor),
+            prevButton.heightAnchor.constraint(equalTo: nextButton.heightAnchor),
+            prevButton.widthAnchor.constraint(equalTo: prevButton.heightAnchor),
+            
+            self.bodyView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            self.bodyView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            self.bodyView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            self.bodyView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
 
         let lpgr = UILongPressGestureRecognizer(target: self, action: #selector(NFXRawBodyDetailsController.copyLabel))
         self.bodyView.addGestureRecognizer(lpgr)
         
         switch bodyType {
             case .request:
-                self.bodyView.text = self.selectedModel.getRequestBody() as String
+                self.originalString = self.selectedModel.getRequestBody() as String
             default:
-                self.bodyView.text = self.selectedModel.getResponseBody() as String
+                self.originalString = self.selectedModel.getResponseBody() as String
         }
+        self.bodyView.text = originalString
         
-        self.view.addSubview(self.bodyView)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        keyboarbUpdates(notification: notification)
+    }
+    
+    @objc private func keyboardWillChange(notification: NSNotification) {
+        keyboarbUpdates(notification: notification)
+    }
+    
+    private func keyboarbUpdates(notification: NSNotification) {
+        guard let userInfo = notification.userInfo as? [String: Any] else {
+            return
+        }
+        guard let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double,
+            let keyboardEndFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+            let animationCurve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt
+            else {
+                return
+        }
+        let padding = paddingForFrame(keyboardEndFrame)
+        let options = UIView.AnimationOptions(rawValue: (animationCurve as UInt) << 16)
+        bodyView.contentInset.bottom = padding
+    }
+
+    
+    private func paddingForFrame(_ frame: CGRect) -> CGFloat {
+        let endYPosition = frame.origin.y
+        let keyboardHeight = frame.height
+        let windowHeight = UIApplication.shared.keyWindow!.frame.height
+        let padding = endYPosition >= windowHeight ? 0.0 : keyboardHeight
+        
+        return padding
     }
 
     @objc fileprivate func copyLabel(lpgr: UILongPressGestureRecognizer) {
@@ -65,6 +169,84 @@ class NFXRawBodyDetailsController: NFXGenericBodyDetailsController
 
     @objc fileprivate func dismissCopyAlert() {
         copyAlert?.dismiss(animated: true) { [weak self] in self?.copyAlert = nil }
+    }
+    
+    @objc fileprivate func nextTapped() {
+        guard currentResult < searchResults.count - 1 else {
+            nextButton.isEnabled = false
+            return
+        }
+        
+        prevButton.isEnabled = true
+        
+        currentResult += 1
+        let result = searchResults[currentResult]
+        scrollTo(result: result)
+                
+        if currentResult == searchResults.count - 1 {
+            nextButton.isEnabled = false
+        }
+    }
+    
+    @objc fileprivate func prevTapped() {
+        guard currentResult > 0 else {
+            prevButton.isEnabled = false
+            return
+        }
+        
+        nextButton.isEnabled = true
+        
+        currentResult -= 1
+        let result = searchResults[currentResult]
+        scrollTo(result: result)
+
+        if currentResult == 0 {
+            prevButton.isEnabled = false
+        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard let original = originalString else {
+            return
+        }
+        
+        timer?.invalidate()
+        
+        let attributed = NSMutableAttributedString(string: original, attributes: [
+            .foregroundColor: UIColor.NFXGray44Color(),
+            .font: UIFont.NFXFont(size: 13)
+        ])
+        
+        guard let regex = try? NSRegularExpression(pattern: searchText, options: .caseInsensitive) else {
+            bodyView.attributedText = attributed
+            prevButton.isEnabled = false
+            nextButton.isEnabled = false
+            searchResults = []
+            currentResult = 0
+            return
+        }
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { [weak self] t in
+            guard let self = self else { return }
+            self.searchResults = regex.matches(in: original, options: [], range: NSRange(location: 0, length: original.utf16.count))
+            for match in self.searchResults.enumerated() {
+                if match.offset == 0 {
+                    self.scrollTo(result: match.element)
+                }
+                attributed.addAttribute(.backgroundColor, value: UIColor.black.withAlphaComponent(0.4), range: match.element.range)
+            }
+            self.prevButton.isEnabled = false
+            self.nextButton.isEnabled = self.searchResults.count > 1
+            self.currentResult = 0
+            self.bodyView.attributedText = attributed
+        })
+    }
+    
+    private func scrollTo(result: NSTextCheckingResult) {
+        let glyphRange = bodyView.layoutManager.glyphRange(forCharacterRange: result.range, actualCharacterRange: nil)
+        let rect = bodyView.layoutManager.boundingRect(forGlyphRange: glyphRange, in: bodyView.textContainer)
+        let contentOffset = CGPoint(x: 0, y: max(rect.origin.y - 60, 0))
+        bodyView.setContentOffset(contentOffset, animated: true)
     }
 }
 
